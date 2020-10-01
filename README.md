34cb8f9f48e1d117ef8353ad9a0127f9790f0dd4
NaughtyPhoton <nathanstrong.fx@gmail.com>
2020-09-30 19:34:42 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# uc-js-masterclass
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/uc-js-masterclass)
---
+++ index.js
@@ -0,0 +1,10 @@
+// Import stylesheets
+import './style.css';
+
+import i from './1-functional-programming/1-immutable-vs-mutable.js';
+
+i()
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
